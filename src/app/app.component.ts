ad167259cf39f9bcf232627addf4e0728d5bd72d
Sergey Pashnin <sspinit88@gmail.com>
2019-11-16 13:29:13 +0300

    добавил isLoggerIn$ / out$ + логику отображения. Дополнен сервис авторизации

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { UserModel } from './model/user.model';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  constructor() {
+  isLoggedIn$: Observable<boolean>;
+  isLoggedOut$: Observable<boolean>;
 
+  constructor(
+    private authService: AuthService,
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.isLoggedIn$ = this.authService.isLoggerIn$;
+    this.isLoggedOut$ = this.authService.isLoggedOut$;
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,14 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { UserModel } from '../model/user.model';
+import { map } from 'rxjs/operators';
+
+
+export const ANONYMOUS_USER: UserModel = {
+  id: undefined,
+  email: '',
+};
 
 @Injectable(
   // {providedIn: 'root'}
 )
 export class AuthService {
 
-  user$: Observable<UserModel>;
+  private subject = new BehaviorSubject<UserModel>(ANONYMOUS_USER);
+
+  user$ = this.subject.asObservable();
+
+  isLoggerIn$: Observable<boolean> = this.user$.pipe(map((user: UserModel) => !!user.id));
+
+  isLoggedOut$: Observable<boolean> = this.isLoggerIn$.pipe(map(isLoggedIn => !isLoggedIn));
 
   constructor(
     private _http: HttpClient,
