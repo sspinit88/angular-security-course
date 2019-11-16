2a27240eec7eb9b658f6d1b9b637225a8e474bd5
Sergey Pashnin <sspinit88@gmail.com>
2019-11-16 20:34:41 +0300

    подготовка

--- server/create-user.route.ts
+++ server/create-user.route.ts
@@ -1,32 +1,29 @@
-
-import {Request, Response} from "express";
-import {db} from "./database";
-import {USERS} from "./database-data";
+import { Request, Response } from 'express';
+import { db } from './database';
+import { USERS } from './database-data';
 import * as argon2 from 'argon2';
-import {validatePassword} from "./password-validation";
+import { validatePassword } from './password-validation';
 
 
+export function createUser(req: Request, res: Response) {
 
-export function createUser(req: Request, res:Response) {
+  const credentials = req.body;
 
-    const credentials = req.body;
+  const errors = validatePassword(credentials.password);
 
-    const errors = validatePassword(credentials.password);
+  if (errors.length > 0) {
+    res.status(400).json({ errors });
+  } else {
+    argon2.hash(credentials.password)
+      .then(passwordDigest => {
 
-    if (errors.length > 0) {
-        res.status(400).json({errors});
-    }
-    else {
-        argon2.hash(credentials.password)
-            .then(passwordDigest => {
+        const user = db.createUser(credentials.email, passwordDigest);
 
-                const user = db.createUser(credentials.email, passwordDigest);
+        console.log(USERS);
 
-                console.log(USERS);
+        res.status(200).json({ id: user.id, email: user.email });
 
-                res.status(200).json({id:user.id, email:user.email});
-
-            });
-    }
+      });
+  }
 
 }
